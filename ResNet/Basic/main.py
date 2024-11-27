e4188a3c8d5c1d9ecb0fd17cd0879f2607449369
Michał <m01lejza@gmail.com>
2024-11-27 22:57:04 +0100

    Deleted All ResNet model.

--- ResNet/Basic/main.py
+++
@@ -1,9 +0,0 @@
-from Models.DataHandlers.Mnist.Mnist import MnistDataset
-
-
-if __name__ == '__main__':
-    train = MnistDataset("ByClass/train-images", "ByClass/train-labels")
-
-    train.printSize()
-    train.plotImage(0)
-    train.plotEightImages()
