94550e6977ccf17cc8f99e0c94741a503393c4f3
Michał <m01lejza@gmail.com>
2024-11-27 22:44:17 +0100

    Adding __init__ file to DataHandlers

---
+++ DataHandlers/__init__.py
@@ -0,0 +1,36 @@
+import os
+import platform
+
+# acknowledging different OS
+OS_NAME = platform.system()
+
+# Creating Datapath to Datasets according to OS
+if OS_NAME == "Windows":  # Windows
+    DATA_PATH = os.path.expanduser("~\\Desktop\\Data")
+elif OS_NAME == "Darwin":  # macos
+    DATA_PATH = os.path.expanduser("")
+else:
+    raise ValueError("Your Operating Systems is not supported!\nTry Windows/MacOS")
+
+# List of currently all available Datasets on this Machine
+AVAILABLE_DATASETS = [_sets for _sets in os.listdir(DATA_PATH) if os.path.isdir(os.path.join(DATA_PATH, _sets))]
+
+
+def get_dataset_path(dataset_name: str) -> str:
+    """
+    Method checks if there exists a Dataset and returns its path.
+    :param dataset_name: Name of Dataset as string.
+    :return: Path to dataset as string.
+    """
+    if dataset_name not in AVAILABLE_DATASETS:
+        raise ValueError(f"Dataset: {dataset_name} is currently not available. {AVAILABLE_DATASETS}")
+    return os.path.join(DATA_PATH, dataset_name)
+
+
+# Exported elements of module
+__all__ = [
+    "OS_NAME",
+    "DATA_PATH",
+    "AVAILABLE_DATASETS",
+    "get_dataset_path",
+]
