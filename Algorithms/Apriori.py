6c2b7f2c8e14ccb3a39bdb4cdf3974a469c06575
Michał <m01lejza@gmail.com>
2024-12-05 23:13:03 +0100

    Update DataHandlers ver.1.3

--- Algorithms/Apriori.py
+++ Algorithms/Apriori.py
@@ -1,7 +1,20 @@
-import time
+import numpy as np
 
 
 class Apriori:
-    def __init__(self, min_support, min_confidence):
-        self.min_support = min_support
-        self.min_confidence = min_confidence
+    def __init__(self, min_support: float, min_confidence: float, data: dict) -> None:
+        self.__min_support: float = min_support
+        self.__min_confidence: float = min_confidence
+        self.__data: dict = data
+        self.__rules: list = []
+        self.__support_set: dict = {}
+
+    def get_rules(self) -> list:
+        return self.__rules
+
+    def get_support_set(self) -> dict:
+        return self.__support_set
+
+    def __str__(self) -> str:
+        return f'Apriori Algorithm:\nMin support: {self.__min_support}\nMin confidence: {self.__min_confidence}'
+
