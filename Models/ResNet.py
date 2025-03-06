56204853c5fe36239ab8d2ddc51a0f507a6c1d49
MichalLejza <m01lejza@gmail.com>
2025-03-06 11:13:53 +0100

    Update ResNet 1.0

--- Models/ResNet.py
+++ Models/ResNet.py
@@ -1,13 +1,67 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from Models.Blocks.BasicBlock import BasicBlock
+from Models.Blocks.BottleneckBlock import BottleneckBlock
 
 class ResNet(nn.Module):
-    def __init__(self):
+    def __init__(self, bottleneck: bool=False, basic: bool=False, layers: list=None, num_classes: int=10):
         super(ResNet, self).__init__()
+        if bottleneck == basic or list is None or len(list) != 4:
+            raise Exception('Wrong Arguments Provided for ResNet Class')
 
-    def __make_layer(self):
-        pass
+        self.in_channels = 64
+        self.expansion = 1 if basic else 4
+
+        # First conv Layer, identical to all ResNet Models
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.batchnorm1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Main blocks of ResNet
+        if bottleneck:
+            self.layer1 = self.__make_layer_bottleneck(64, layers[0])
+            self.layer2 = self.__make_layer_bottleneck(128, layers[1])
+            self.layer3 = self.__make_layer_bottleneck(256, layers[2])
+            self.layer4 = self.__make_layer_bottleneck(512, layers[3])
+        if basic:
+            self.layer1 = self.__make_layer_basic(64, layers[0])
+            self.layer2 = self.__make_layer_basic(128, layers[1])
+            self.layer3 = self.__make_layer_basic(256, layers[2])
+            self.layer4 = self.__make_layer_basic(512, layers[3])
+
+        # End layer
+        self.avgpool = nn.AvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * self.expansion, num_classes)
+
+    def __make_layer_basic(self, out_channels, num_blocks):
+        layers: list = []
+        layers.append(BasicBlock(in_channels=self.in_channels, out_channels=out_channels, stride=1))
+
+        self.in_channels = out_channels * self.expansion
+
+        for i in range(1, num_blocks):
+            layers.append(BasicBlock(in_channels=self.in_channels, out_channels=out_channels, stride=2))
+
+        return nn.Sequential(*layers)
+
+    def __make_layer_bottleneck(self, in_channels, out_channels):
+        layers = []
+        return nn.Sequential(*layers)
 
     def foward(self, x):
-        pass
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
